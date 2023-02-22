2e160315a85051f4a31547adc7d703e3fba2fce5
DynamiteBob17 <jan.mlinaric@gmail.com>
2023-02-22 21:27:14 +0100

    Initial commit

---
+++ Periodic Table/element.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PSQL="psql --username=db17 --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1")
+else
+  ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1' OR name = '$1'")
+fi
+
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+echo "$ELEMENT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME
+do
+  PROPERTIES=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  echo "$PROPERTIES" | while read ATOMIC_MUMBER BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE_ID
+  do
+    TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
+    TYPE_FORMATTED=$(echo "$TYPE" | sed -r 's/^ *| *$//g')
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE_FORMATTED, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+  done
+done
